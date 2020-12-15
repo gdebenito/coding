c17a6d8bd2e8400c314f2df8fb5c085c09e9d497
Gonzalo De Benito <gdebenitocassado@gmail.com>
2020-12-15 20:30:38 +0100

    completed day 10

--- adventOfCode2020/day10/main2.go
+++ adventOfCode2020/day10/main2.go
@@ -4,91 +4,78 @@ import (
 	"fmt"
 	"io/ioutil"
 	"math"
+	"sort"
 	"strconv"
 	"strings"
 )
 
-type Graph struct {
-	length            int
-	possibleJumps     map[int][]int
-	possibleJumpsBack map[int][]int
-}
-
 func main() {
-	input, max := parseNumbersFromInput("./test2.txt")
+	input, max := parseNumberFromInput("./input.txt")
 	// value := 1 + calcPath(0, max, input, 0)
-	input[0] = true
-	g := generateGraph(input)
-	fmt.Println(g.possibleJumps)
-	fmt.Println(g.possibleJumpsBack)
-	fmt.Println(max)
-	count := g.countPaths(0, max, 0)
+	fmt.Println(input)
+	diff := differences(input, max)
+	fmt.Println(diff)
+	countOnes(diff)
 
-	fmt.Println(count)
 }
 
-func generateGraph(input map[int]bool) *Graph {
-	g := Graph{
-		length:            len(input),
-		possibleJumps:     make(map[int][]int),
-		possibleJumpsBack: make(map[int][]int),
-	}
-	for k := range input {
-		g.possibleJumpsBack[k] = make([]int, 0)
-	}
-
-	for k := range input {
-		arr := make([]int, 0)
-
-		for i := 1; i <= 3; i++ {
-
-			if _, isInMap := input[k+i]; isInMap {
-				arr = append(arr, k+i)
-
-				aux := g.possibleJumpsBack[k+i]
-				aux = append(aux, k)
-				g.possibleJumpsBack[k+i] = aux
-			}
-
+func countOnes(num []int) {
+	comb := make(map[int]int)
+	comb[1] = 1
+	comb[2] = 1
+	comb[3] = 2
+	comb[4] = 4
+	comb[5] = 7
+	counter := 1
+	v := 1
+	i := 0
+	for {
+		if num[i] == 1 {
+			counter++
+		} else {
+			v = v * comb[counter]
+			counter = 1
 		}
+		i++
 
-		g.possibleJumps[k] = arr
-	}
-
-	return &g
-
-}
-
-func (g *Graph) countPaths(current int, destination int, count int) int {
-	if current == destination {
-		// fmt.Printf("%d %d", current, destination)
-		count++
-	} else {
-		possibleJumps := g.possibleJumps[current]
-		// fmt.Println(possibleJumps)
-		for _, nextJump := range possibleJumps {
-			// fmt.Printf("%d %d\n", nextJump, destination)
-			count = g.countPaths(nextJump, destination, count)
+		if i == len(num) {
+			v = v * comb[counter]
+			counter = 1
+			break
 		}
 	}
-	return count
+	fmt.Println(v)
 }
 
-func parseNumbersFromInput(input string) (map[int]bool, int) {
+func differences(num []int, max int) []int {
+	sort.Ints(num)
+	fmt.Println(num)
+	diff := make([]int, len(num))
+	last := num[0]
+	for i := 1; i < len(num); i++ {
+		current := num[i]
+		diff[i] = current - last
+		last = current
+	}
+	return diff[1:]
+}
+
+func parseNumberFromInput(input string) ([]int, int) {
 	content, err := ioutil.ReadFile(input)
 	if err != nil {
 		panic(err)
 	}
 
 	lines := strings.Split(string(content), "\n")
-	numbers := make(map[int]bool)
+	numbers := make([]int, 1)
+	numbers[0] = 0
 	max := 0
 	for _, line := range lines {
 		value, err := strconv.Atoi(line)
 		if err != nil {
 			panic(err)
 		}
-		numbers[value] = true
+		numbers = append(numbers, value)
 		max = int(math.Max(float64(value), float64(max)))
 	}
 	return numbers, max
